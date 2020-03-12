83052c61f87e5fca56cfd8468108257d6a0f8957
Mike Conrad <mike@enxo.co>
2020-03-12 13:19:42 -0400

    Updated background and contentscript.  Now clicking icon will enable/disable kittyfi

--- background.js
+++ background.js
@@ -1,10 +1,37 @@
-// this is the background code...
+chrome.tabs.onUpdated.addListener(function (tab) {
+	chrome.storage.sync.get('kitty', function (data) {
+		var enabled = data.kitty;
+		var icon = (enabled === true) ? 'icon-128.png' : 'icon-128-disabled.png'
+		chrome.browserAction.setIcon({
+			path: icon,
+			tabId: tab.id
+		});
 
-// listen for our browerAction to be clicked
-chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.ib, {
-		file: 'inject.js'
 	});
+})
+chrome.browserAction.onClicked.addListener(function (tab) {
+
+	chrome.storage.sync.get('kitty', function (data) {
+		var enabled = !data.kitty;
+
+		chrome.storage.sync.set({
+			'kitty': enabled
+		});
+	});
+	if (typeof enabled === 'undefined') {
+		var enabled = false
+	}
+	if (enabled === true) {
+		chrome.tabs.executeScript(tab.id, {
+			file: 'cats.js'
+		})
+		chrome.tabs.executeScript(tab.id, {
+			file: 'inject.js'
+		});
+	} else {
+
+		chrome.tabs.update(tab.id, { url: tab.url })
+	}
+
 });
 
